f9b641c6d51f9e81937013d06e4d66edf4b26278
Rv <rvebreo777@gmail.com>
2024-10-29 07:26:50 +0800

    Dynamin Name in log in

--- students/views.py
+++ students/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
 from django.views.decorators.cache import never_cache
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.cache import never_cache
-from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
 @never_cache
 @login_required
 def studentAppointment(request):
-    return render(request, 'appointment.html')
+    return render(request, 'appointment.html', {'user': request.user})
 @never_cache
 @login_required
 def studentMessages(request):
-    return render(request, 'messages.html')
+    return render(request, 'messages.html', {'user': request.user})
 @never_cache
 @login_required
 def studentSchedule(request):
-    return render(request, 'schedule.html')
+    return render(request, 'schedule.html', {'user': request.user})
 @never_cache
 @login_required
 def studentSettings(request):
-    return render(request, 'settings.html')
+    return render(request, 'settings.html', {'user': request.user})
